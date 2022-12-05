a9a5b1bb7a68db7be9e98f8744d12ff01a3964f2
joshua10342 <119874356+joshua10342@users.noreply.github.com>
2022-12-05 20:15:10 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgrPlugin from 'vite-plugin-svgr'
-import { resolve } from 'path'
+import { join } from 'path'
+import mdx from 'vite-plugin-mdx'
+import pages from 'vite-plugin-react-pages'
 
 export default defineConfig({
-  appType: 'mpa',
   plugins: [
     react(),
     svgrPlugin({
       svgrOptions: {
-        icon: true
+        icon: true,
       }
-    })
+    }),
+    mdx(),
+    pages({
+      pagesDir: join(__dirname, 'src/pages'),
+    }),
   ],
-  build: {
-    outDir: resolve(__dirname, 'dist'),
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        contact: resolve(__dirname, 'contact.html'),
-        market: resolve(__dirname, 'market.html'),
-        stats: resolve(__dirname, 'market', 'stats.html')
-      }
-    }
-  }
 })
